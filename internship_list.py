1c9f654d496b617c898164f3990700f4646c8b11
Jimkelly Percine <jimkellypercine@brandeis.edu>
2023-02-02 17:59:38 -0500

    output excel values to README.md

--- internship_list.py
+++ internship_list.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import numpy as np
 
 # Load the Excel file into a Pandas dataframe
 df = pd.read_excel('Internship_list.xlsx')
 
 # Display the first 4 rows of the dataframe
-print(df.head(4))
+print(df.head(7))
+
+
+f= open("README.md", "a")
+f.write(df.to_string())
+f.close()
+#np.savetxt('./README.md', df.values)
