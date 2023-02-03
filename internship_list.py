7cf59783656486fc171e36e357c5820c536e28a0
mcap <64444406+Mcaplovitz@users.noreply.github.com>
2023-02-02 19:39:57 -0500

    comments

--- internship_list.py
+++ internship_list.py
@@ -7,7 +7,7 @@ df = pd.read_excel('Internship_list.xlsx')
 # Display the first 4 rows of the dataframe
 print(df.head(7))
 
-
+#load the dataframe into a markdown file
 f= open("README.md", "w")
 f.write("List of internships within the COSI Department \n" + df.to_markdown())
 f.close()
