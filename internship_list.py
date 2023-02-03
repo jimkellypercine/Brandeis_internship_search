93a53057c8fa5ef3acfd7984bca40b92bc3ec562
Jimkelly Percine <jimkellypercine@brandeis.edu>
2023-02-02 19:49:45 -0500

    updated internship_list.py

--- internship_list.py
+++ internship_list.py
@@ -6,9 +6,10 @@ df = pd.read_excel('Internship_list.xlsx')
 
 # Display the first 4 rows of the dataframe
 print(df.head(7))
+df.reset_index(drop=True)
 
 
-f= open("README.md", "a")
+f= open("README.md", "w")
 f.write(df.to_string())
 f.close()
 #np.savetxt('./README.md', df.values)
