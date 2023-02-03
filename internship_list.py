71aa195e442487d6e374d8fc0b04cded50755636
Jimkelly Percine <jimkellypercine@brandeis.edu>
2023-02-02 22:21:25 -0500

    Updated

--- internship_list.py
+++ internship_list.py
@@ -7,7 +7,6 @@ df = pd.read_excel('Internship_list.xlsx')
 # Display the first 4 rows of the dataframe
 print(df.head(30))
 df.reset_index(drop=True)
-
 #load the dataframe into a markdown file
 f= open("README.md", "w")
 f.write("List of internships within the COSI Department \n" + df.to_markdown())
