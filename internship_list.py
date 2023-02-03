74c58bff97c13f06ee431a5fc7ac72eb3ae01497
Jimkelly Percine <jimkellypercine@brandeis.edu>
2023-02-03 10:31:01 -0500

    updated list and udr details

--- internship_list.py
+++ internship_list.py
@@ -10,9 +10,9 @@ df.reset_index(drop=True)
 
 #load the dataframe into a markdown file
 f= open("README.md", "w")
-markdown = '![Alt text](brandeispic.png)' 
-f.write(markdown + "\n")
-f.write("COSCI HANDBOOK: https://docs.google.com/document/d/1skDckhZJi6OQxjraKNVEZfxflTE4Jy8kxCJtpNu3O1Y/edit?usp=gmail \n" + "\n")
-f.write("***********************************\n       CURRENT UDRS             \n***********************************\n* Yalda Mauj                     *\n* Jimkelly Percine               *\n* Gabrielle Pile                 *\n* Gianna Everette                *\n* Sydney Cohen                   *\n***********************************\n")
-f.write("List of internships within the COSI Department \n" + df.to_markdown())
+markdown = '![Brandeis COSCI](brandeispic.png)' 
+f.write(markdown + "\n" + "\n")
+f.write("BRANDEIS COSCI HANDBOOK: https://docs.google.com/document/d/1skDckhZJi6OQxjraKNVEZfxflTE4Jy8kxCJtpNu3O1Y/edit?usp=gmail \n" + "\n")
+f.write("***********************************\nCURRENT UDRS\n\n* Yalda Mauj                     *\n* Jimkelly Percine               *\n* Gabrielle Pile                 *\n* Gianna Everette                *\n* Sydney Cohen                   *\n***********************************\n")
+f.write("List of internships within the COSI Department \n" + df.to_markdown(floatfmt=".2f"))
 f.close()
