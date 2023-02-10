4243fe91eba2f599c108c9af7c0851bdcf61e96e
Jimkelly Percine <jimkellypercine@brandeis.edu>
2023-02-10 15:41:50 -0500

    updated message output

--- internship_list.py
+++ internship_list.py
@@ -7,17 +7,18 @@ df = pd.read_excel('Internship_list.xlsx')
 print(df)
 df = df.reset_index(drop=True)
 df.index = df.index + 1
-
 UDRS = ["Yalda Mauj", "Jimkelly Percine", "Gabrielle Pile", "Gianna Everette", "Sydney Cohen"]
 
+df['Application List'] = df['Application List'].str.slice(start=0, stop=20)
 print(df)
 
 #load the dataframe into a markdown file
 f= open("README.md", "w")
 markdown = '![](images/brandeispic.png)' 
 f.write(markdown)
-f.write("\n \n BRANDEIS COSCI HANDBOOK: https://docs.google.com/document/d/1skDckhZJi6OQxjraKNVEZfxflTE4Jy8kxCJtpNu3O1Y/edit?usp=gmail \n" + "\n")
+f.write("WELCOME TO THE BRANDEIS INTERNSHIP LIST \n\n GET IN TOUCH WITH BRANDEIS STUDENTS WITH INTERNSHIP EXPERIENCE TO LAND ONE YOURSELF \n\n")
 f.write(" \n CURRENT UDRS \n " + "\n".join(["- " + UDRS for UDRS in UDRS]))
+f.write("\n \n BRANDEIS COSCI HANDBOOK: https://docs.google.com/document/d/1skDckhZJi6OQxjraKNVEZfxflTE4Jy8kxCJtpNu3O1Y/edit?usp=gmail \n" + "\n")
 f.write("\n \n Connect with Contributors\n https://docs.google.com/document/d/1XWu8NFlqvPPQSOIbt93K7tdSejl2mA1KmV7HSjAZLMs/edit?usp=sharing")
 f.write("\n \n List of internships within the COSI Department \n" + df.to_markdown(floatfmt=".2f"))
 f.close()
